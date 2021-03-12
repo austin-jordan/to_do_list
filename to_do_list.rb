50cb9205e5829397b450155838ae33ab3965e2a5
Austin Jordan <austinj@nulogy.com>
2021-03-12 14:38:32 -0500

    Add ability to change priority

--- to_do_list.rb
+++ to_do_list.rb
@@ -1,15 +1,19 @@
-# Priority
+# Change Priority -- Focus on this next
 # Due date
-# Mark as completed
 # Description
 tasks = []
 while true
-  puts "Available commands: (a)dd (d)elete"
+  puts "Available commands: (a)dd (d)elete (c)hange priority"
   print "> "
   input = gets.chomp
   system "clear"
   if input[0] == "a"
     tasks << input[2..-1]
+  elsif input[0] == "c"
+    puts "What priority do you want to give it?"
+    print"> "
+    priority_input = gets.chomp
+    tasks.insert(priority_input.to_i, tasks.delete_at(tasks.index(input[2..-1])))
   else
     tasks.delete(input[2..-1])
   end
