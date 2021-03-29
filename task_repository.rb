6ba4b216be9f21d7d17f414196846a1b9ed2db6b
Austin Jordan <austinj@nulogy.com>
2021-03-29 16:53:52 -0400

    Extract #store

--- task_repository.rb
+++ task_repository.rb
@@ -2,7 +2,6 @@ require 'yaml/store'
 
 class TaskRepository
   def self.read_all
-    store = YAML::Store.new "tasks.yaml"
     tasks = nil
     store.transaction do
       tasks = store["tasks"] || []
@@ -11,9 +10,12 @@ class TaskRepository
   end
 
   def self.save(tasks)
-    store = YAML::Store.new "tasks.yaml"
     store.transaction do
       store["tasks"] = tasks
     end
+  end
+
+  def self.store
+    @store ||= YAML::Store.new("tasks.yaml")
   end
 end
