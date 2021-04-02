825193df6a0504c3ba7a0fed15b5c419f2c1ac72
Austin Jordan <austinj@nulogy.com>
2021-04-02 17:25:32 -0400

    Add ability to add tasks

--- to_do_list.rb
+++ to_do_list.rb
@@ -2,3 +2,15 @@
 # Description
 # Clear list
 # Prevent duplicate tasks
+tasks = []
+while true
+  puts "Available commands: (a)dd task"
+  input = gets.chomp
+  if input[0] == "a"
+    tasks << input[2..-1]
+  end
+  puts
+  puts "-- Tasks --"
+  puts tasks
+  puts
+end
