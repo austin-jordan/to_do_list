31e78747f4fcde98a57dbd20afbb13841ff82b4b
Austin Jordan <austinj@nulogy.com>
2021-04-02 17:29:14 -0400

    Add ability to delete tasks

--- to_do_list.rb
+++ to_do_list.rb
@@ -4,10 +4,14 @@
 # Prevent duplicate tasks
 tasks = []
 while true
-  puts "Available commands: (a)dd task"
+  puts "Available commands: (a)dd task (d)elete task"
   input = gets.chomp
-  if input[0] == "a"
-    tasks << input[2..-1]
+  command = input[0]
+  task = input[2..-1]
+  if command == "a"
+    tasks << task
+  elsif command == "d"
+    tasks.delete(task)
   end
   puts
   puts "-- Tasks --"
