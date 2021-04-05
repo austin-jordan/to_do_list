33faf7d7d6766a0f6a965fc3685e27a3936ed0f0
Austin Jordan <austinj@nulogy.com>
2021-04-05 15:16:37 -0400

    Add ToDoList class and main method

--- to_do_list.rb
+++ to_do_list.rb
@@ -4,32 +4,39 @@
 # Prevent duplicate tasks
 require 'yaml/store'
 
-store = YAML::Store.new "tasks.yaml"
-tasks = nil
-store.transaction do
-  tasks = store["tasks"] || []
+class ToDoList
+  def main
+    store = YAML::Store.new "tasks.yaml"
+    tasks = nil
+    store.transaction do
+      tasks = store["tasks"] || []
+    end
+
+    while true
+      puts "Available commands: (a)dd task (c)hange priority (d)elete task"
+      input = gets.chomp
+      command = input[0]
+      task = input[2..-1]
+      if command == "a"
+        tasks << task
+      elsif command == "d"
+        tasks.delete(task)
+      elsif command == "c"
+        puts "What priority do you want to give it"
+        priority_input = gets.chomp
+        tasks.delete_at(tasks.index(task))
+        tasks.insert(priority_input.to_i - 1, task)
+      end
+      puts
+      puts "-- Tasks --"
+      puts tasks
+      puts
+      store.transaction do
+        store["tasks"] = tasks
+      end
+    end
+  end
 end
 
-while true
-  puts "Available commands: (a)dd task (c)hange priority (d)elete task"
-  input = gets.chomp
-  command = input[0]
-  task = input[2..-1]
-  if command == "a"
-    tasks << task
-  elsif command == "d"
-    tasks.delete(task)
-  elsif command == "c"
-    puts "What priority do you want to give it"
-    priority_input = gets.chomp
-    tasks.delete_at(tasks.index(task))
-    tasks.insert(priority_input.to_i - 1, task)
-  end
-  puts
-  puts "-- Tasks --"
-  puts tasks
-  puts
-  store.transaction do
-    store["tasks"] = tasks
-  end
-end
+to_do_list = ToDoList.new()
+to_do_list.main
