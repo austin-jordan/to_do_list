f6629498ea5dfe048f05353dca9d1ff1a34a7a61
Austin Jordan <austinj@nulogy.com>
2021-04-16 16:02:59 -0400

    Add delete functionality

--- to_do_list.rb
+++ to_do_list.rb
@@ -5,12 +5,14 @@ class ToDoList
     while true
       puts '-- Tasks --'
       puts tasks
-      puts "Available commands -- (a)dd task"
+      puts "Available commands -- (a)dd task (d)elete task"
       input = gets.chomp
       command = input[0]
       task = input[1..-1]
       if command == 'a'
         tasks << task
+      elsif command == "d"
+        tasks.delete(task)
       end
     end
   end
